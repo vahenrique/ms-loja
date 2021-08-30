0f3309f30192c3bc3a9b7228c9abb53202d7fdc9
Vinicius <vinic.ah@gmail.com>
2021-08-29 21:48:24 -0300

    correção na migration de item

--- catalogo/src/main/resources/db/migration/V20210829204900__create-table-item.sql
+++ catalogo/src/main/resources/db/migration/V20210829204900__create-table-item.sql
@@ -2,5 +2,6 @@ create table item (
 	id BINARY(16) NOT NULL PRIMARY KEY,
 	nome varchar(60) NOT NULL,
     valor decimal(20,2) NOT NULL DEFAULT 0.00,
-    categoria_id BINARY(16) NOT NULL FOREIGN KEY REFERENCES categoria(id), 
+    categoria_id BINARY(16) NOT NULL,
+    FOREIGN KEY (categoria_id) REFERENCES categoria(id)
 );
