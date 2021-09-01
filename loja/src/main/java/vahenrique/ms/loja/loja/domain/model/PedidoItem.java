a90d6e54c1a8a2f397a34dfe0681ac95a1adc583
Vinicius <vinic.ah@gmail.com>
2021-09-01 20:24:14 -0300

    criando modelagem da tabela de itens do pedido

--- loja/src/main/java/vahenrique/ms/loja/loja/domain/model/PedidoItem.java
+++ loja/src/main/java/vahenrique/ms/loja/loja/domain/model/PedidoItem.java
@@ -2,31 +2,25 @@ package vahenrique.ms.loja.loja.domain.model;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.time.OffsetDateTime;
-import java.util.Set;
 import java.util.UUID;
 
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.GenericGenerator;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import vahenrique.ms.loja.loja.domain.enumeration.StatusPedido;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
 @Entity
-public class Pedido implements Serializable {
+public class PedidoItem implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
@@ -35,13 +29,11 @@ public class Pedido implements Serializable {
 	@GeneratedValue(generator = "uuid2")
 	@GenericGenerator(name = "uuid2", strategy = "uuid2")
 	private UUID id;
-	private OffsetDateTime dtGeracao;
-	@Enumerated(EnumType.STRING)
-	private StatusPedido status;
-	private BigDecimal valorFrete;
 	@ManyToOne
 	@JoinColumn(name = "cliente_id", nullable = false)
-	private Cliente cliente;
-	@OneToMany(mappedBy = "pedido")
-	private Set<PedidoItem> pedidoItems;
+	private Pedido pedido;
+	private UUID catalogoItemId;
+	private String nome;
+	private BigDecimal quantidade;
+	private BigDecimal valorUnitario;
 }
