6cb270bdf0c9af7edaa33b5348bd1987e52cc0d0
Vinicius <vinic.ah@gmail.com>
2021-08-31 22:37:03 -0300

    tratamento das mensagens de validação

---
+++ loja/src/main/java/vahenrique/ms/loja/loja/api/model/ErroDto.java
@@ -0,0 +1,17 @@
+package vahenrique.ms.loja.loja.api.model;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ErroDto {
+
+	private Integer status;
+	private OffsetDateTime dataHora;
+	private String titulo;
+	List<ErroCampoDto> campos;
+}
