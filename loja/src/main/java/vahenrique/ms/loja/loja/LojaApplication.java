7c4f4d854c8e2a406e2c38b3ced3ed82e2ed2b0e
Vinicius <vinic.ah@gmail.com>
2021-09-04 14:53:08 -0300

    correções no uso do discovery client

--- loja/src/main/java/vahenrique/ms/loja/loja/LojaApplication.java
+++ loja/src/main/java/vahenrique/ms/loja/loja/LojaApplication.java
@@ -2,8 +2,10 @@ package vahenrique.ms.loja.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LojaApplication {
 
 	public static void main(String[] args) {
--- transportadora/src/main/java/vahenrique/ms/loja/transportadora/TransportadoraApplication.java
+++ transportadora/src/main/java/vahenrique/ms/loja/transportadora/TransportadoraApplication.java
@@ -2,8 +2,10 @@ package vahenrique.ms.loja.transportadora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TransportadoraApplication {
 
 	public static void main(String[] args) {
